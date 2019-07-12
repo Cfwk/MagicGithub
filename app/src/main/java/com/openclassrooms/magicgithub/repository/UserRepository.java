fa3b689e3573b4192ea1eae10f4c4e8e4b53cc9c
Sindrosa <skiti1996@live.fr>
2019-07-12 16:11:53 +0200

    Delete useless lines

--- app/src/main/java/com/openclassrooms/magicgithub/repository/UserRepository.java
+++ app/src/main/java/com/openclassrooms/magicgithub/repository/UserRepository.java
@@ -1,12 +1,10 @@
 package com.openclassrooms.magicgithub.repository;
-
 import com.openclassrooms.magicgithub.api.ApiService;
 import com.openclassrooms.magicgithub.api.FakeApiServiceGenerator;
 import com.openclassrooms.magicgithub.model.User;
 
 import java.util.LinkedList;
 import java.util.List;
-import java.util.ListIterator;
 
 public class UserRepository implements  ApiService{
 
@@ -26,7 +24,6 @@ public class UserRepository implements  ApiService{
         int nombreAleatoire = 0 + (int)(Math.random() * ((FakeApiServiceGenerator.FAKE_USERS_RANDOM.size()-1 - 0) + 1));
         System.out.println(nombreAleatoire);
         User RandomUser = FakeApiServiceGenerator.FAKE_USERS_RANDOM.get(nombreAleatoire);
-
         users.add(RandomUser);
     }
 
